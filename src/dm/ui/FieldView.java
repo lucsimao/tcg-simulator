f5734217ed33466e076dcc552b5d3b9fc821142d
Lucas Simão <lucas64_64@hotmail.com>
2017-05-01 16:41:17 -0300

    Adicionadas todas as cartas ao field

--- src/dm/ui/FieldView.java
+++ src/dm/ui/FieldView.java
@@ -34,17 +34,26 @@ public class FieldView extends JPanel {
 	private  final static int height = 400;
 	private final static int width =640 ;
 	private final String field_path = "field.png";
-	private static final int monster_x = 147;
-	private static final int monster1_y = 100 ;
-	private static final int monster2_y = 239;
-	private static final int extra_left_x =monster_x+354 ;
-	private static final int deck1_y = monster2_y+95;
-
-	private static final int extra_right_x = 74;
+	private static final int monster_x = 146;
+	private static final int monster2_y = 100 ;
+	private static final int monster1_y = 239;
+	private static final int extra_right_x =monster_x+355 ;
+	private static final int extra_left_x = 74;
+	private static final int deck1_y = monster1_y+95;
 //	private static final int graveyard1;
 //	private static final int graveyard1;
 //	private static final int deck1;
 //	private static final int deck2;
+
+	private static final int spell1_y = monster1_y + 62;
+
+	private static final int spell2_y = monster2_y - 62;
+
+	private static final int graveyard1_y =deck1_y - 64;
+
+	private static final int deck2_y = 2;
+
+	private static final int graveyard2_y = deck2_y + 64;
 	
 	public static void main(String args[]) throws IOException{
 		JFrame f = new JFrame();
@@ -73,26 +82,69 @@ public class FieldView extends JPanel {
 		setMaximumSize(new Dimension(width, height));
 		ImageMixer im = new ImageMixer();
 		BufferedImage bufferedImage = ImageIO.read(new File(FilesConstants.TEXTURES_PATH + field_path));
+		//Monster 1
+		for(int i=0;i<5;i++){
+			bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
+					new Dimension(56,62), monster_x+70*i +i+1,  monster1_y);
+		}
+
+		//Spell 1
+		for(int i=0;i<5;i++){
+			bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
+					new Dimension(56,62), monster_x+70*i +i+1,  spell1_y);
+		}
+		
+		//Monster 2
+		for(int i=0;i<5;i++){
+			bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
+					new Dimension(56,62), monster_x+70*i +i+1,  monster2_y);
+		}
+		
+		//Spell 2
+		for(int i=0;i<5;i++){
+			bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
+					new Dimension(56,62), monster_x+70*i +i+1,  spell2_y);
+		}
+		
 		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
-				new Dimension(56,62), monster_x,  monster1_y);
+				new Dimension(56,62), extra_left_x,  deck1_y);
 		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
-				new Dimension(56,62),  monster_x + 70, monster2_y);
+				new Dimension(56,62), extra_left_x,  graveyard1_y);
 		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
-				new Dimension(56,62),  monster_x + 141,  monster2_y);
+				new Dimension(56,62), extra_right_x,  deck1_y);
 		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
-				new Dimension(56,62),  monster_x + 142 + 70,  monster2_y);
+				new Dimension(56,62), extra_right_x,  graveyard1_y);
+		
 		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
-				new Dimension(56,62),  monster_x + 143 + 70 + 70,  monster2_y);
+				new Dimension(56,62), extra_left_x,  deck2_y);
 		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
-				new Dimension(56,62), monster_x, monster2_y);
+				new Dimension(56,62), extra_left_x,  graveyard2_y);
 		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
-				new Dimension(56,62), extra_left_x,deck1_y);
+				new Dimension(56,62), extra_right_x,  deck2_y);
 		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
-				new Dimension(56,62), extra_right_x,deck1_y);
-		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
-				new Dimension(56,62),  monster_x,monster2_y+62);
-		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
-				new Dimension(56,62),  monster_x,monster1_y-62);
+				new Dimension(56,62), extra_right_x,  graveyard2_y);
+		
+//		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
+//				new Dimension(56,62),  monster_x + 70, monster2_y);
+//		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
+//				new Dimension(56,62),  monster_x + 141,  monster2_y);
+//		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
+//				new Dimension(56,62),  monster_x + 142 + 70,  monster2_y);
+//		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
+//				new Dimension(56,62),  monster_x + 143 + 70 + 70,  monster2_y);
+//		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
+//				new Dimension(56,62), monster_x, monster2_y);
+//		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
+//				new Dimension(56,62), extra_left_x,deck1_y);
+//		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
+//				new Dimension(56,62), extra_right_x,deck1_y);
+//		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
+//				new Dimension(56,62), extra_right_x,deck1_y-64);
+//		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
+//				new Dimension(56,62),  monster_x,monster2_y-62);
+//		bufferedImage = im.mixImages(bufferedImage,new File("D:/magonego.png"),new Dimension(width,height),
+//				new Dimension(56,62),  monster_x,monster1_y+62);
+		
 //		BufferedImage bufferedImagecard = ImageIO.read(new File("D:/magonego.png"));
 		ImageTransform it = new ImageTransform();
 //		ImageIcon image = new ImageIcon(it.perspectiveTransform(bufferedImage,8,width,height*3/5));
@@ -103,15 +155,7 @@ public class FieldView extends JPanel {
 		JLabel lblNewLabel = new JLabel(image);
 		lblNewLabel.setBounds(0, 0, width, height);
 		add(lblNewLabel);
-//		JLabel lblNewLabel2 = new JLabel(card);
-//		JLabel lblNewLabel3 = new JLabel(card);
-//		lblNewLabel.add(lblNewLabel2);
-//		lblNewLabel.add(lblNewLabel3);
-//		lblNewLabel2.setBounds(148,100, 56, 62);
-//		lblNewLabel3.setBounds(218,100, 54, 62);
-//		lblNewLabel3.setBounds(288,100, 56, 62);
-//		lblNewLabel3.setBounds(288,210, 54, 58);
-//		add(lblNewLabel2);
+
 	}
 
 }
