069fabb0bca1f351a53771ae844ba651262eb406
Lucas Simão <lucas64_64@hotmail.com>
2017-06-29 20:15:15 -0300

    iniciada a hand

---
+++ src/dm/ui/HandView.java
@@ -0,0 +1,52 @@
+/** 
+* @author Simão 
+* @version 0.1 - 29 de jun de 2017
+* 
+*/
+package dm.ui;
+
+import java.awt.GridLayout;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+import dm.cards.MonsterNormalCard;import dm.cards.abstracts.Card;
+
+public class HandView extends JPanel {
+	
+	private final static int height = 120;
+	private final static int width = 640;
+	
+	public static void main(String args[]){
+		JFrame f = new JFrame();
+		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		f.setUndecorated(true);
+		f.setVisible(true);
+
+
+		HandView fv = new HandView();
+
+		
+		f.getContentPane().add(fv);
+		fv.setFocusable(true);
+		fv.requestFocusInWindow();
+		f.setBounds(100, 100, width, height);
+	}
+	
+	private int number;
+	private int selected;
+	
+	public HandView(){
+	
+		setLayout(new GridLayout(1, 5,3,3));
+		ImageIcon icon = new ImageIcon(new MonsterNormalCard(3).getPicture());
+		CardImage c = new CardImage(icon);
+		add(c,100,100);
+		
+	}
+	
+}
