a3d5fd8ef2760f0e0b1aa4f1c758f8a3235f65e1
Simão <lucas64_64@hotmail.com>
2018-05-23 10:02:33 -0300

    Criado os testes para a BattlePhase TODO

---
+++ test/dm/tests/phases/BattlePhaseTests.java
@@ -0,0 +1,39 @@
+package dm.tests.phases;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import dm.exceptions.PlayerCannotDrawException;
+import dm.game.phases.BattlePhase;
+
+public class BattlePhaseTests extends PhaseTests {
+
+	private BattlePhase battlePhase;
+	
+	@Test
+	public void attackAMonster() {
+		//TODO
+	}
+	
+	@Test 
+	public void attackWithEachMonster() {
+		//TODO
+	}
+	
+	@Test 
+	public void tryToAttackTwiceIfMonsterCant() {
+		//TODO
+	}
+	
+	@Test 
+	public void attackTwiceIfMonsterCan() {
+		//TODO
+	}
+	
+	@Test 
+	public void attackIfMonsterCantAttack() {
+		//TODO
+	}
+	
+}
