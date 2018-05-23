0d7a18305ba19f2ef78a04e9abb4d6a0910c0e95
Simão <lucas64_64@hotmail.com>
2018-05-22 21:04:52 -0300

    Criada a classe StandbyPhase

---
+++ src/dm/game/phases/StandByPhase.java
@@ -0,0 +1,11 @@
+package dm.game.phases;
+
+import dm.game.Player;
+
+public class StandByPhase extends Phase{
+
+	public StandByPhase(Player player) {
+		super(player);
+	}
+
+}
---
+++ test/dm/tests/phases/StandByPhaseTests.java
@@ -0,0 +1,9 @@
+package dm.tests.phases;
+
+import dm.game.phases.StandByPhase;
+
+public class StandByPhaseTests extends PhaseTests {
+
+	private StandByPhase standByPhase;
+		
+}
